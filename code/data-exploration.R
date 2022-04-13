531ced0ce39a4cfcefbc1bbf02a41682a12a4891
amal-kadri <98130059+amal-kadri@users.noreply.github.com>
2022-04-13 16:37:37 -0500

    added files and some more wrangling

---
+++ code/data-exploration.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(ggplot2)
+
+sat_district_data_class_2020 <- read.csv("data/sat-district-data-class-2020.csv")
+disadvantaged = sat_district_data_class_2020 %>%
+  filter(Group == "Economically Disadvantaged")
+censusu_poverty_estimates_2020 <- read_csv("data/censusu-poverty-estimates-2020.csv", skip = 3)
+poverty_abridged_tx =  censusu_poverty_estimates_2020[,1:10] %>% 
+  rename(CntyName =  Name) %>% 
+  rename('PostalCode'='Postal Code') %>%
+  rename('Poverty_Percent_All_Ages' = 'Poverty Percent, All Ages') %>%
+  filter(PostalCode == 'TX') %>% filter(CntyName != 'Texas')
+merged = inner_join(disadvantaged, poverty_abridged_tx, by = "CntyName")
+
+colnames(merged)
+
+#generate basic residuals, Math scores
+lm1 =  lm(Math ~ Poverty_Percent_All_Ages, data = merged)
+math_hat = predict(lm1,merged)
+merged_math =  merged%>%
+  mutate(math_pred =  math_hat) %>%
+  mutate(resid = (Math - math_pred))
+
+plot(lm1) #it's a straight line :(
+
+ggplot(merged_math, aes(x=Poverty_Percent_All_Ages, y=Math)) +
+  geom_smooth()
+
+ggplot(merged_math, aes(x=Poverty_Percent_All_Ages, y=resid)) +
+  geom_point() +
+  geom_smooth()
+
+#TSI readiness
+lm2 =  lm(Above_TSI_Both_Rate ~ Poverty_Percent_All_Ages, data = merged)
+math_hat = predict(lm2,merged)
+merged_TSI =  merged%>%
+  mutate(TSI_pred =  math_hat) %>%
+  mutate(resid = (Above_TSI_Both_Rate - TSI_pred))
+
+plot(lm2) #it's a straight line :(
+
+ggplot(merged_TSI, aes(x=Poverty_Percent_All_Ages, y=Above_TSI_Both_Rate)) +
+  geom_smooth()
+
+ggplot(merged_TSI, aes(x=Poverty_Percent_All_Ages, y=resid)) +
+  geom_point() +
+  geom_smooth()
+
+merged_TSI = merged_TSI %>%
+  mutate(over_TSI =  ifelse(resid > 50, 1, 0))
--- data-exploration.R
+++ data-exploration.R
@@ -1,13 +1,30 @@
 library(tidyverse)
 library(ggplot2)
 
-sat_district_data_class_2020 <- read.csv("sat-district-data-class-2020.csv")
+sat_district_data_class_2020 <- read.csv("data/sat-district-data-class-2020.csv")
 disadvantaged = sat_district_data_class_2020 %>%
   filter(Group == "Economically Disadvantaged")
-censusu_poverty_estimates_2020 <- read_csv("censusu-poverty-estimates-2020.csv", skip = 3)
+censusu_poverty_estimates_2020 <- read_csv("data/censusu-poverty-estimates-2020.csv", skip = 3)
 poverty_abridged_tx =  censusu_poverty_estimates_2020[,1:10] %>% 
   rename(CntyName =  Name) %>% 
-  rename('PostalCode'='Postal Code') %>% 
+  rename('PostalCode'='Postal Code') %>%
+  rename('Poverty_Percent_All_Ages' = 'Poverty Percent, All Ages') %>%
   filter(PostalCode == 'TX') %>% filter(CntyName != 'Texas')
 merged = inner_join(disadvantaged, poverty_abridged_tx, by = "CntyName")
 
+colnames(merged)
+lm1 =  lm(Math ~ Poverty_Percent_All_Ages, data = merged)
+math_hat = predict(lm1,merged)
+merged =  merged%>%
+  mutate(math_pred =  math_hat) %>%
+  mutate(resid = (Math - math_pred))
+
+plot(lm1) #it's a straight line :(
+
+ggplot(merged, aes(x=Poverty_Percent_All_Ages, y=Math)) +
+  geom_smooth()
+
+ggplot(merged, aes(x=Poverty_Percent_All_Ages, y=resid)) +
+    geom_smooth()
+  
+  

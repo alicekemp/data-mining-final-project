c9f95ab7c9e245149dba05eed096dcf430abc2aa
amal-kadri <98130059+amal-kadri@users.noreply.github.com>
2022-05-08 23:32:10 -0500

    plots made

--- code/plot_testing.R
+++ code/plot_testing.R
@@ -28,24 +28,59 @@ gridExtra::grid.arrange(arrangeGrob(FreeElig_mean_plot,
                         top="Partial Dependence Plots for All Schools")
 
 
-#########################under
+#########################over
 feats_over = rf_over_vip[,1]
-plot_list_over = list()
 for (i in feats_over){
   plot = pdp::partial(rf_over, pred.var = i, plot = TRUE, plot.engine = "ggplot2") + 
-    ggtitle(paste("Partial Dependence Plot of ", i)) + 
+    ggtitle(paste(i)) + 
+    xlab(paste(i)) + 
+    ylab("Predicted Resid")
+  plot_name = paste(i,"plot_over", sep = "_")
+  assign(plot_name, plot)
+}
+description_over = textGrob(
+"Models Generated using Random Forest Regression,
+used to predict the Residuals of the inital Linear Model.
+These variables carry the most Importance in identifying 
+the source of the Variation in educational outcomes
+for Overperforming School Districts"
+)
+gridExtra::grid.arrange(arrangeGrob(st_pct_gifted_plot_over, 
+                                    teach_turnover_rate_plot_over,
+                                    Change_2010.20_pct_plot_over,
+                                    RUC.code_plot_over,
+                                    staff_pct_aux_plot_over,
+                                    description_over),
+                        top="Partial Dependence Plots for Overperforming Schools")
+
+##########################under
+feats_under = rf_under_vip[,1]
+for (i in feats_under){
+  plot = pdp::partial(rf_under, pred.var = i, plot = TRUE, plot.engine = "ggplot2") + 
+    ggtitle(paste(i)) + 
     xlab(paste(i)) + 
     ylab("Predicted Resid")
   plot_name = paste(i,"plot_under", sep = "_")
   assign(plot_name, plot)
-  plot_list_over = c(plot_list_over,plot_name)
 }
+description_over = textGrob(
+"Models Generated using Random Forest Regression,
+used to predict the Residuals of the inital Linear Model.
+These variables carry the most Importance in identifying 
+the source of the Variation in educational outcomes
+for Underperforming School Districts"
+)
+gridExtra::grid.arrange(arrangeGrob(avg_salary_central_plot_under, 
+                                    RUC.code_plot_under,
+                                    st_pct_speced_plot_under,
+                                    avg_salary_prof_plot_under,
+                                    teach_pct_advdegrees_plot_under,
+                                    description_over),
+                        top="Partial Dependence Plots for Underperforming Schools")
 
 
 
-
-
-
+#############RF Resid Plot
 yhat_forest =  predict(rf,newdata=mod_test)
 mod_test = cbind(mod_test,yhat_forest)
 
@@ -56,5 +91,6 @@ ggplot(mod_test, aes(x=yhat_forest,y=lin_resid))+
   xlab("Predicted Residual (randomForest)") +
   ylab("Actual Residuals (Linear Model)")
 
+#############RF outcomes plot
 
 

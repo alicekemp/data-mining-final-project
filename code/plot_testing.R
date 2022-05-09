d1191870e1c5c2546e7e7512e330ff94f9717830
amal-kadri <98130059+amal-kadri@users.noreply.github.com>
2022-05-08 22:31:22 -0500

    trying to make flexible plots

--- code/plot_testing.R
+++ code/plot_testing.R
@@ -1,9 +1,8 @@
 load("all_objects.RData")
 library(ggplot2)
 library(gridExtra)
-# top 5 partial dependence plots 
+# top 5 partial dependence plots ALL
 feats_all = importance_table[,1]
-par(mfrow = c(3,2))
 for (i in feats_all){
   
   plot = pdp::partial(rf, pred.var = i, plot = TRUE, plot.engine = "ggplot2") + 
@@ -29,6 +28,24 @@ gridExtra::grid.arrange(arrangeGrob(FreeElig_mean_plot,
                                     exp_pct_cartecheduc_plot,
                                     description),
                         top="Partial Dependence Plots for All Schools")
+
+
+#########################under
+feats_over = rf_over_vip[,1]
+plot_list_over = list()
+for (i in feats_over){
+  plot = pdp::partial(rf_over, pred.var = i, plot = TRUE, plot.engine = "ggplot2") + 
+    ggtitle(paste("Partial Dependence Plot of ", i)) + 
+    xlab(paste(i)) + 
+    ylab("Predicted Resid")
+  plot_name = paste(i,"plot_under", sep = "_")
+  assign(plot_name, plot)
+  plot_list_over = c(plot_list_over,plot_name)
+}
+
+
+
+
 
 
 yhat_forest =  predict(rf,newdata=mod_test)
